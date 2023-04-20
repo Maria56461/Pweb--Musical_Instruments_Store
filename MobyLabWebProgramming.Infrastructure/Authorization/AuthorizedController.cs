a5a35e6f21443046deac2a49ccb2b3acb95f64a0
MARIA ALEXANDRA BARBU <mariabarbu27@yahoo.com>
2023-04-20 09:03:42 +0300

    Correct Claim Added To Token

--- MobyLabWebProgramming.Infrastructure/Authorization/AuthorizedController.cs
+++ MobyLabWebProgramming.Infrastructure/Authorization/AuthorizedController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using Ardalis.SmartEnum;
 using Microsoft.AspNetCore.Mvc;
 using MobyLabWebProgramming.Core.DataTransferObjects;
 using MobyLabWebProgramming.Core.Enums;
@@ -32,7 +33,7 @@ public abstract class AuthorizedController : ControllerBase
         var userId = enumerable.Where(x => x.Type == ClaimTypes.NameIdentifier).Select(x => Guid.Parse(x.Value)).FirstOrDefault();
         var email = enumerable.Where(x => x.Type == ClaimTypes.Email).Select(x => x.Value).FirstOrDefault();
         var name = enumerable.Where(x => x.Type == ClaimTypes.Name).Select(x => x.Value).FirstOrDefault();
-        var role = (UserRoleEnum) enumerable.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).FirstOrDefault();
+        var role = enumerable.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).FirstOrDefault();
 
         _userClaims = new(userId, name, email, role);
 
--- MobyLabWebProgramming.Infrastructure/Authorization/UserClaims.cs
+++ MobyLabWebProgramming.Infrastructure/Authorization/UserClaims.cs
@@ -5,5 +5,5 @@ namespace MobyLabWebProgramming.Infrastructure.Authorization;
 /// <summary>
 /// This record is used to store the claims extracted from the JWT
 /// </summary>
-public record UserClaims(Guid Id, string? Name, string? Email, UserRoleEnum? Role);
+public record UserClaims(Guid Id, string? Name, string? Email, string? Role);
 
